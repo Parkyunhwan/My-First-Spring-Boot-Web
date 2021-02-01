e7e41bfbe25aaa823d5b54a40eac1b26574d9417
yunhwan <pyh8618@gmail.com>
2021-02-02 02:39:25 +0900

    SpringBoot 1.5.4 버전 시큐리티 + OAuth2 구현 & JAVA 11 -> 8 Downgrade (facebook /, google X, Kakao X)

--- src/main/java/com/yunhwan/springbootcommunityweb/web/BootWebApplication.java
+++ src/main/java/com/yunhwan/springbootcommunityweb/web/BootWebApplication.java
@@ -5,41 +5,53 @@ import com.yunhwan.springbootcommunityweb.web.domain.enums.BoardType;
 import com.yunhwan.springbootcommunityweb.web.domain.User;
 import com.yunhwan.springbootcommunityweb.web.repository.BoardRepository;
 import com.yunhwan.springbootcommunityweb.web.repository.UserRepository;
+import com.yunhwan.springbootcommunityweb.web.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class BootWebApplication {
+public class BootWebApplication extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
+
     public static void main(String[] args) {
         SpringApplication.run(BootWebApplication.class, args);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
+    }
+
     @Bean
-    public CommandLineRunner runner(UserRepository userRepository,
-                                    BoardRepository boardRepository) throws Exception{
+    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
         return (args) -> {
             User user = userRepository.save(User.builder()
                     .name("havi")
                     .password("test")
-                    .email("havi@gamil.com")
+                    .email("havi@gmail.com")
                     .createdDate(LocalDateTime.now())
-                    .build()
-            );
+                    .build());
 
             IntStream.rangeClosed(1, 200).forEach(index ->
                     boardRepository.save(Board.builder()
-                            .title("게시글" + index)
-                            .subTitle("순서" + index)
-                            .content("콘텐츠")
+                            .title("게시글"+index)
+                            .subTitle("순서"+index)
+                            .content("컨텐츠")
                             .boardType(BoardType.free)
                             .createdDate(LocalDateTime.now())
                             .updatedDate(LocalDateTime.now())
-                            .build())
+                            .user(user).build())
             );
         };
     }
--- src/main/java/com/yunhwan/springbootcommunityweb/web/service/BoardService.java
+++ src/main/java/com/yunhwan/springbootcommunityweb/web/service/BoardService.java
@@ -2,6 +2,7 @@ package com.yunhwan.springbootcommunityweb.web.service;
 
 import com.yunhwan.springbootcommunityweb.web.domain.Board;
 import com.yunhwan.springbootcommunityweb.web.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,14 +18,15 @@ public class BoardService {
     }
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.
-                    getPageNumber() - 1, pageable.getPageSize());
-
+        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
         return boardRepository.findAll(pageable);
     }
 
     public Board findBoardByIdx(Long idx) {
-        return boardRepository.findById(idx).orElse(new Board()); // 옵션널 반환값..? 없다면 새 보드 객체 반환?
-        // orElse는 null이 아닐때는 반환하지 않지만 new ...은 실행된다. -> orElseGet()으로 사용하면 해결가
+        return boardRepository.findOne(idx);
+    }
+
+    public Board saveAndUpdateBoard(Board board) {
+        return boardRepository.save(board);
     }
 }
